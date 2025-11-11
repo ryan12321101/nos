fcd719199e0ffacb42724fd0eaecd99300cb87fe
ryan12321101 <pocolocoreisoficial@gmail.com>
2025-11-10 22:42:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -31,3 +31,54 @@ playBtn.addEventListener("click", () => {
     playBtn.textContent = "▶";
   }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Frases românticas
+const frases = [
+  "Você é o meu lugar preferido no mundo inteiro.",
+  "Desde que te conheci, tudo ficou mais bonito.",
+  "Nada é mais doce do que te ter por perto.",
+  "Você é a paz que o meu coração sempre procurou.",
+  "Com você, o amor virou o meu destino. 💖"
+];
+
+let index = 0;
+const textBox = document.getElementById("loveText");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+function trocarTexto(novoIndex) {
+  textBox.classList.add("fade-out");
+  setTimeout(() => {
+    textBox.textContent = frases[novoIndex];
+    textBox.classList.remove("fade-out");
+  }, 400);
+}
+
+prevBtn.addEventListener("click", () => {
+  index = (index - 1 + frases.length) % frases.length;
+  trocarTexto(index);
+});
+
+nextBtn.addEventListener("click", () => {
+  index = (index + 1) % frases.length;
+  trocarTexto(index);
+});
